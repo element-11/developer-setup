1da9f5bd3e0d3219c3261eed2a507a6b1d85c5d8
Ben Ellis <ben@element11.com>
2020-10-05 16:25:44 -0500

    Add separate script for subs

---
+++ sub.ps1
@@ -0,0 +1,85 @@
+#
+#	Description: Added Innovation Developer Boxstarter Script
+#	Author: Mark Whisler
+#	Company: Added Innovation
+# 
+#	Installs core company and development tools for a .NET developer and VS Enterprise subscriber
+#
+#
+#	Big thanks to https://github.com/microsoft/windows-dev-box-setup-scripts
+#
+
+Disable-UAC
+$Boxstarter.RebootOk = $true
+$Boxstarter.NoPassword = $false
+$Boxstarter.AutoLogin = $true
+$ConfirmPreference = "None" #ensure installing PowerShell modules don't prompt on needed dependencies
+
+# Get the base URI path from the ScriptToCall value
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", " ")
+$helperUri = $helperUri.TrimEnd("'", " ")
+$helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf("/"))
+$helperUri += "/scripts"
+
+Write-host "Script base URI is $helperUri"
+
+function ExecuteScript {
+    Param ([string]$script)
+    Write-host "Executing $helperUri/$script ..."
+	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
+}
+
+#	
+#	Set up Windows
+#
+ExecuteScript "SystemSettings.ps1";
+ExecuteScript "RemoveWindowsApps.ps1";
+
+#
+#	Set up Added Core
+#
+
+choco install adobereader     --limitoutput
+choco install 7zip			  --limitoutput    
+choco install filezilla		  --limitoutput        
+choco install sysinternals    --limitoutput
+choco install googlechrome
+choco install fiddler --install-arguments "/D=C:\Program Files\Telerik\Fiddler"	--limitoutput
+choco install postman			--limitoutput    
+choco install dotpeek			--limitoutput
+choco install vscode
+choco install microsoft-windows-terminal
+refreshenv
+
+
+Update-SessionEnvironment
+
+#ExecuteScript "VisualStudio.ps1"
+
+if (Test-PendingReboot) { Invoke-Reboot }
+
+Update-SessionEnvironment
+
+#
+#	Setup IIS
+#
+ExecuteScript "IIS.ps1"
+
+if (Test-PendingReboot) { Invoke-Reboot }
+
+#
+#	Setup SQL Server
+#
+choco install sql-server-management-studio --limitoutput  
+
+if (Test-PendingReboot) { Invoke-Reboot }
+#
+#	Resume and Install Updates
+#
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
