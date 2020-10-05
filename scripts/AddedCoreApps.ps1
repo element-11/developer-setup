4b2c95dfac17a286855c05977df17a5727dd9e63
Ben Ellis <ben@element11.com>
2020-10-05 16:25:30 -0500

    Clean up

--- scripts/AddedCoreApps.ps1
+++ scripts/AddedCoreApps.ps1
@@ -2,20 +2,11 @@ choco install adobereader     --limitoutput
 #choco install brave    --limitoutput
 #choco install notepadplusplus --limitoutput    
 choco install 7zip			  --limitoutput    
-choco install lastpass		  --limitoutput
+#choco install lastpass		  --limitoutput
 choco install filezilla		  --limitoutput        
 #choco install ditto			  --limitoutput    
 choco install microsoft-teams --limitoutput
-#choco install greenshot		  --limitoutput   
 choco install slack			  --limitoutput
 choco install zoom
-#choco install bluejeansapp	  --limitoutput
 choco install sysinternals    --limitoutput
-choco install toggl
 choco install googlechrome
-choco install firefox
-choco install snagit
-
-# Ben's
-choco install amazon-music
-choco install steam
